46edd427c8d9d11c0810eef81e7a77cb9c4746f1
Charagundla Hemanth Sai <hemanthsai.ch456@gmail.com>
2026-08-03 19:53:06 +0530

    Time: 0 ms (100%), Space: 8.6 MB (95.8%) - LeetHub

---
+++ 0171-excel-sheet-column-number/0171-excel-sheet-column-number.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int titleToNumber(string columnTitle) {
+        long long col = 0;
+        for (char ch : columnTitle)
+        {
+            col = col * 26 + (ch - 'A' + 1);
+        }
+        return (int)col;
+    }
+};
